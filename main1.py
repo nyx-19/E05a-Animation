5b89a00af80fb3b0af9ab9636ddeeb469065b05e
Morton <caigmort@iu.edu>
2019-09-25 18:52:39 -0400

    finished exercise

--- main1.py
+++ main1.py
@@ -10,12 +10,12 @@ If Python and Arcade are installed, this example can be run from the command lin
 python -m arcade.examples.move_mouse
 """
 
-import arcade
+import arcade     #what tells python that we are wanting to run this on arcade and not in the python terminal
 
-#sets the desired pixel width and heigh for the screen that apprears
+#sets the desired pixel width and heigh for the screen that apprears 
 SCREEN_WIDTH = 640         
 SCREEN_HEIGHT = 480        
-SCREEN_TITLE = "Move Mouse Example"
+SCREEN_TITLE = "Move Mouse Example"   #titles the arcade window
 
 
 class Ball:
@@ -29,12 +29,12 @@ class Ball:
 
     def draw(self):
         """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)  #tells the program exactly what the numbers and values we will enter later will apply to when we create the ball
 
 
 class MyGame(arcade.Window):
 
-    def __init__(self, width, height, title):
+    def __init__(self, width, height, title):  #takes information from above that has been defined and places it together.
 
         # Call the parent class's init function
         super().__init__(width, height, title)
@@ -43,41 +43,41 @@ class MyGame(arcade.Window):
         # So we just see our object, not the pointer.
         self.set_mouse_visible(False)
 
-        arcade.set_background_color(arcade.color.ASH_GREY)
+        arcade.set_background_color(arcade.color.ASH_GREY) #sets the background color given the files that arcade already has to chose from
 
         # Create our ball
         self.ball = Ball(50, 50, 15, arcade.color.AUBURN)
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
-        arcade.start_render()
-        self.ball.draw()
+        arcade.start_render()   #what tells the arcade window to appear when we run it
+        self.ball.draw()        #what puts the ball onto the screen when we run the program
 
-    def on_mouse_motion(self, x, y, dx, dy):
+    def on_mouse_motion(self, x, y, dx, dy):      #i am confused where the dx and dy velocities are used in the lines below
         """ Called to update our objects. Happens approximately 60 times per second."""
-        self.ball.position_x = x
-        self.ball.position_y = y
+        self.ball.position_x = x        #tracks the ball's x location and assigns the x position to wherever the x imput from the mouse is
+        self.ball.position_y = y        #tracks the ball's y location and assigns the y position to whereever the y imput from the mouse is
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button.
         """
-        print(f"You clicked button number: {button}")
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.ball.color = arcade.color.BLACK
+        print(f"You clicked button number: {button}") #communicates to the user when a specific button is pushed and tells which one
+        if button == arcade.MOUSE_BUTTON_LEFT:          # defines that if the left mouse button is pushed, the ball will change color to black
+            self.ball.color = arcade.color.BLACK        # this line is explained in line above
 
     def on_mouse_release(self, x, y, button, modifiers):
         """
         Called when a user releases a mouse button.
         """
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.ball.color = arcade.color.AUBURN
+        if button == arcade.MOUSE_BUTTON_LEFT: #when the button is let go, the left click on mouse, it returns the color back to it's original state
+            self.ball.color = arcade.color.AUBURN   #line explained above
 
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE) #it's allll contected
+    arcade.run()     #tells arcade to start up
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":      #i'm guessing this is a nessesarry self-awareness function so that it knows if this program is called main, then it should be running main
     main()
--- main2.py
+++ main2.py
@@ -9,12 +9,13 @@ If Python and Arcade are installed, this example can be run from the command lin
 python -m arcade.examples.move_keyboard
 """
 
-import arcade
+import arcade           #what makes python know we want to be running this on arcade
 
+#sets the desired pixel height and width of the arcade screen that is desplayed
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-SCREEN_TITLE = "Move Keyboard Example"
-MOVEMENT_SPEED = 3
+SCREEN_TITLE = "Move Keyboard Example"  #titles the window
+MOVEMENT_SPEED = 3                      # sets the desired speed of the ball
 
 
 class Ball:
@@ -30,7 +31,7 @@ class Ball:
 
     def draw(self):
         """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)  #explains to the program what the values of the numbers and text we enter later for the ball in their place that it will apply to these functions
 
     def update(self):
         # Move the ball
@@ -49,7 +50,7 @@ class Ball:
 
         if self.position_y > SCREEN_HEIGHT - self.radius:
             self.position_y = SCREEN_HEIGHT - self.radius
-
+            #these are all explained by the comment left by the creator
 
 class MyGame(arcade.Window):
 
@@ -62,42 +63,42 @@ class MyGame(arcade.Window):
         # So we just see our object, not the pointer.
         self.set_mouse_visible(False)
 
-        arcade.set_background_color(arcade.color.ASH_GREY)
+        arcade.set_background_color(arcade.color.ASH_GREY)     #applies a color to the screen with the pre-existing color options in arcade
 
         # Create our ball
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
+        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)  #sets all the desired values into the funtion slots we talk about earlier on line 34
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
-        arcade.start_render()
-        self.ball.draw()
+        arcade.start_render()   #what tells the arcade window to appear when we run it
+        self.ball.draw()        #what puts the ball onto the screen when we run the program
 
     def update(self, delta_time):
-        self.ball.update()
+        self.ball.update()      #gives imput updates to the program as we move this about the screen at what ever delta_time is
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
-            self.ball.change_x = -MOVEMENT_SPEED
+            self.ball.change_x = -MOVEMENT_SPEED      #defines that when the left key is pushed, it will make the ball move negatively along the x axis
         elif key == arcade.key.RIGHT:
-            self.ball.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.ball.change_y = MOVEMENT_SPEED
+            self.ball.change_x = MOVEMENT_SPEED       #defines that when the right key is pushed, it will make the ball move positively along the x axis
+        elif key == arcade.key.UP:    
+            self.ball.change_y = MOVEMENT_SPEED       #defines that when the up key is pushed, it will make the ball move positively along the y axis
         elif key == arcade.key.DOWN:
-            self.ball.change_y = -MOVEMENT_SPEED
+            self.ball.change_y = -MOVEMENT_SPEED      #defines that when the down key is pushed, it will make the ball move negatively along the y axis
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.change_x = 0
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:     
+            self.ball.change_x = 0                              #this defines that if the ball moves at all along the x axis, the x value will change accordingly
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.change_y = 0
+            self.ball.change_y = 0                              #this defines that if the ball moves at all along the y axis, the y value will chnage accordingly
 
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)   #what creates the window given the values we defined for it earlier
+    arcade.run()                             #what makes the whole program start up
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  #this is also probably a needed self aware funtion that tells the program if this is called main, then it should be running main
     main()
